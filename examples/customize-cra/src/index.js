935e119b253aaf03bd93811a00e932288e9ff6dd
Zohaib Ijaz <mzohaib.qc@gmail.com>
2020-05-19 07:48:19 +0500

    Added gh-pages to deploy on github pages

--- examples/customize-cra/src/index.js
+++ examples/customize-cra/src/index.js
@@ -7,7 +7,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <Theme /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
