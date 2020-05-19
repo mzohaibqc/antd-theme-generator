524c7e855db3c8c0f24fa18adc4a221c953a5608
Zohaib Ijaz <mzohaib.qc@gmail.com>
2020-05-19 07:47:42 +0500

    Updates

---
+++ precache-manifest.be9a4083fece0c0264c411247175e84f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1041e1d5581f51ba098ff06fd9b4d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd93d18c7cb8f958464a",
+    "url": "/static/css/2.e392defd.chunk.css"
+  },
+  {
+    "revision": "e297895986ae88be0f8f",
+    "url": "/static/css/main.4552cdc5.chunk.css"
+  },
+  {
+    "revision": "dd93d18c7cb8f958464a",
+    "url": "/static/js/2.e78554b1.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/static/js/2.e78554b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e297895986ae88be0f8f",
+    "url": "/static/js/main.5553719d.chunk.js"
+  },
+  {
+    "revision": "aaf411ce36fe10516ed0",
+    "url": "/static/js/runtime-main.b2946e44.js"
+  }
+]);
